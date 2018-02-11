37461fbe56c33aec4868c02649a61ac7b2536ec1
nicissimo97 <nici.regista@gmail.com>
2018-02-11 21:08:33 +0100

    Update it.js

--- plugins/ratio/lang/it.js
+++ plugins/ratio/lang/it.js
@@ -6,21 +6,21 @@
  * Author: 
  */
 
- theUILang.ratios		= "Ratio Groups";
+ theUILang.ratios		= "Gruppi ratio";
  theUILang.ratio		= "RatioGroup";
- theUILang.mnuRatio		= "Set Ratio Group";
+ theUILang.mnuRatio		= "Importa ratio group";
  theUILang.mnuRatioUnlimited	= "No Ratio";
- theUILang.ratioName		= "Name";
+ theUILang.ratioName		= "Nome";
  theUILang.minRatio		= "Min";
  theUILang.maxRatio		= "Max";
  theUILang.ratioUpload		= "UL";
- theUILang.ratioAction		= "Action";
- theUILang.ratioStop		= "Stop";
- theUILang.ratioStopAndRemove	= "Stop & Clear group";
- theUILang.ratioErase		= "Remove";
- theUILang.ratioEraseData	= "Remove data";
- theUILang.maxTime		= "Time";
- theUILang.ratioDefault 	= "Default ratio group";
- theUILang.setThrottleTo	= "Set channel to";
+ theUILang.ratioAction		= "Azione";
+ theUILang.ratioStop		= "Ferma";
+ theUILang.ratioStopAndRemove	= "Ferma e cancella gruppo";
+ theUILang.ratioErase		= "Rimuovi";
+ theUILang.ratioEraseData	= "Rimuovi dati";
+ theUILang.maxTime		= "Ora";
+ theUILang.ratioDefault 	= "Ratio group default";
+ theUILang.setThrottleTo	= "Imposta canale come";
 
 thePlugins.get("ratio").langLoaded();
