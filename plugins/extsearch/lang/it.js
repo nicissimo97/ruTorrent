15ab69fe6028d98f74631bf62c8710e5901d81f2
nicissimo97 <nici.regista@gmail.com>
2018-02-11 03:36:08 +0100

    Update it.js

--- plugins/extsearch/lang/it.js
+++ plugins/extsearch/lang/it.js
@@ -6,36 +6,36 @@
  * Author: 
  */
 
- theUILang.tegMenuLoad			= "Load";
- theUILang.tegMenuOpen			= "Open in browser";
- theUILang.tegMenuDeleteItem		= "Remove item";
- theUILang.tegMenuDelete		= "Remove tag";
- theUILang.tegRefresh			= "Refresh tag";
+ theUILang.tegMenuLoad			= "Carica";
+ theUILang.tegMenuOpen			= "Apri nel browser";
+ theUILang.tegMenuDeleteItem		= "Rimuovi elemento";
+ theUILang.tegMenuDelete		= "Rimuovi etichetta";
+ theUILang.tegRefresh			= "Aggiorna etichetta";
  theUILang.exsGlobal			= "Include into 'All' category";
  theUILang.exsLimit			= "Result limit";
  theUILang.exsEngines			= "Engines";
- theUILang.exsSearch			= "Search";
+ theUILang.exsSearch			= "Cerca";
  theUILang.exsGlobalLimit		= "Common Limitations";
- theUILang.excat			= "Category";
- theUILang.excatall			= "All";
- theUILang.excatmovies			= "Movies";
+ theUILang.excat			= "Categorie";
+ theUILang.excatall			= "Tutti";
+ theUILang.excatmovies			= "Film";
  theUILang.excattv			= "TV";
- theUILang.excatmusic			= "Music";
- theUILang.excatgames			= "Games";
+ theUILang.excatmusic			= "Musica";
+ theUILang.excatgames			= "Giochi";
  theUILang.excatanime			= "Anime";
  theUILang.excatsoftware		= "Software";
- theUILang.excatpictures		= "Pictures";
- theUILang.excatbooks			= "Books";
- theUILang.extPrivate			= "Private";
- theUILang.extPublic			= "Public";
+ theUILang.excatpictures		= "Immagini";
+ theUILang.excatbooks			= "Libri";
+ theUILang.extPrivate			= "Privati";
+ theUILang.extPublic			= "Publici";
  theUILang.exsCookies			= "Format of cookies";
  theUILang.extAllPublic 		= "All Public";
  theUILang.extAllPrivate		= "All Private";
  theUILang.exsMustInstallCookies	= "Cookies plugin is required to work with this engine.";
  theUILang.exsMustInstallLoginMgr	= "LoginMgr plugin is required to work with this engine.";
  theUILang.exsLoginMgr			= "Setup your account in LoginMgr plugin to work with this engine.";
- theUILang.exsURLInfo			= "URL info";
- theUILang.exsURLGUID			= "Description URL";
+ theUILang.exsURLInfo			= "info URL";
+ theUILang.exsURLGUID			= "Descrizione URL";
  theUILang.exsURLHref			= "Download URL";
 
 thePlugins.get("extsearch").langLoaded();
