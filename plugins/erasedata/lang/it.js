f1fc1af6cbae1c35d63208592ae05d51536f8e24
nicissimo97 <nici.regista@gmail.com>
2018-02-11 03:31:25 +0100

    Update it.js

--- plugins/erasedata/lang/it.js
+++ plugins/erasedata/lang/it.js
@@ -6,8 +6,8 @@
  * Author: 
  */
 
- theUILang.Rem_torrents_content_prompt		= "Do you really want to remove the selected torrent(s)? WARNING: This will delete torrent's content.";
- theUILang.Delete_data_with_path		= "Delete Path";
- theUILang.Rem_torrents_with_path_prompt	= "Do you really want to remove the selected torrent(s)? WARNING: This will delete all files in this torrent's current directory.";
+ theUILang.Rem_torrents_content_prompt		= "Vuoi davvero rimuovere il/i torrent selezionato/i? ATTENZIONE: Questa azione eliminerà il file.";
+ theUILang.Delete_data_with_path		= "Cancella Path";
+ theUILang.Rem_torrents_with_path_prompt	= "Vuoi davvero rimuovere il/i torrent selezionato/i? ATTENZIONE: Questa azione eliminerà tutto il contenuto della directory del torrent.";
 
 thePlugins.get("erasedata").langLoaded();
