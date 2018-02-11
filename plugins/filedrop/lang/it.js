cd9fee43c7ff5b34a5079444670af9d59c1e79ed
nicissimo97 <nici.regista@gmail.com>
2018-02-11 03:38:52 +0100

    Update it.js

--- plugins/filedrop/lang/it.js
+++ plugins/filedrop/lang/it.js
@@ -6,8 +6,8 @@
  * Author: 
  */
 
- theUILang.doesntSupportHTML5	= "Plugin filedrop: Your browser does not support HTML5 file uploads. Plugin was disabled.";
- theUILang.tooManyFiles 	= "Plugin filedrop: Too many files. Must be <= ";
- theUILang.fileTooLarge 	= "is too large. Please upload files up to";
+ theUILang.doesntSupportHTML5	= "Plugin filedrop: Il tuo browser non supporta il caricamento file di HTML5. Plugin was disabled.";
+ theUILang.tooManyFiles 	= "Plugin filedrop: Troppi file. Devono essere <= ";
+ theUILang.fileTooLarge 	= "è troppo grosso. Carica file fino a";
 
 thePlugins.get("filedrop").langLoaded();
