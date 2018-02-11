4cca705c92f18afe48315a73c100f96954ae2d49
nicissimo97 <nici.regista@gmail.com>
2018-02-12 00:24:33 +0100

    Update it.js

--- plugins/rssurlrewrite/lang/it.js
+++ plugins/rssurlrewrite/lang/it.js
@@ -6,22 +6,22 @@
  * Author: 
  */
 
- theUILang.rssNewRule		= "New rule";
+ theUILang.rssNewRule		= "Nuova regola";
  theUILang.mnu_rssurlrewrite	= "URL Replacement in RSS";
- theUILang.rssRulesManager	= "Rules Manager";
- theUILang.rssAddRule		= "Add";
- theUILang.rssDelRule		= "Delete";
+ theUILang.rssRulesManager	= "Gestione Regole";
+ theUILang.rssAddRule		= "Aggiungi";
+ theUILang.rssDelRule		= "Rimuovi";
  theUILang.rssCheckRule 	= "?";
- theUILang.rssRulesLegend	= "Rule Settings";
+ theUILang.rssRulesLegend	= "Impostazioni regole";
  theUILang.rssSrcHref		= "If URL of torrent download matches pattern";
  theUILang.rssSrcGuid		= "If URL of torrent description matches pattern";
  theUILang.rssDstHref		= "then replace URL of torrent download with";
  theUILang.rssDstGuid		= "then replace URL of torrent description with";
- theUILang.rssRulesDebug	= "Rule Debug";
+ theUILang.rssRulesDebug	= "Regole Debug";
  theUILang.rssTestString	= "Test";
- theUILang.rssTestResult	= "Result";
- theUILang.rssURLInfo		= "URL info";
- theUILang.rssURLGUID		= "Description URL";
+ theUILang.rssTestResult	= "Risultato";
+ theUILang.rssURLInfo		= "info URL";
+ theUILang.rssURLGUID		= "Descrizione URL";
  theUILang.rssURLHref		= "Download URL";
  theUILang.rssPatternError	= "Error in pattern string.";
  theUILang.rssStatus		= "RSS";
